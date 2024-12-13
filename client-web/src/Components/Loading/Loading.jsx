c452bd86a57ee19e85e496b91b8becb670e9c38e
Ouassima <aboukhairouassima11@gmail.com>
2024-12-13 21:13:55 +0100

    last

--- client-web/src/Components/Loading/Loading.jsx
+++ client-web/src/Components/Loading/Loading.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
 import styles from "./Loading.module.css";
-import 'bootstrap-icons/font/bootstrap-icons.css'; // Assurez-vous que Bootstrap Icons est bien importé
+
 
 function Loading() {
-  const [loading, setLoading] = useState(true); // Utilisation de l'état pour contrôler la visibilité de la page de chargement
-
-  const closeLoadingPage = () => {
-    setLoading(false); // Fermer la page de chargement en changeant l'état
-    console.log("Page de chargement fermée");
-  };
-
-  if (!loading) return null; // Si loading est false, la page de chargement n'est plus affichée
 
   return (
     <div className={styles.overlay}>
