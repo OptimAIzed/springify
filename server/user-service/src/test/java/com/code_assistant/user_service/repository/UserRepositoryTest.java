a0571c2880f1a5cea8352a1d9cece46450b1f462
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2024-12-28 14:20:05 +0100

    removing test profile

--- server/user-service/src/test/java/com/code_assistant/user_service/repository/UserRepositoryTest.java
+++ server/user-service/src/test/java/com/code_assistant/user_service/repository/UserRepositoryTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.Timestamp;
@@ -23,7 +22,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@ActiveProfiles("test")
+
 @SpringBootTest
 public class UserRepositoryTest {
 
